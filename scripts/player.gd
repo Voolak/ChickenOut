cd021f65299ba8d187a7f99787843911e58421db
Voolak <abarzana.perso@gmail.com>
2024-05-09 16:07:19 +0200

    Fire Chick

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var Chick : PackedScene
+
 var speed = 400  # speed in pixels/sec
 
 func _physics_process(delta):
@@ -7,5 +9,13 @@ func _physics_process(delta):
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 	move_and_slide()
+
+func shoot():
+	var b = Chick.instantiate()
+	owner.add_child(b)
+	b.transform = $Marker2D.global_transform
